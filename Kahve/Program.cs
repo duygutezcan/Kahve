184265097e6b75949253a6adb02a9f5ec8fbe159
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-03 08:51:56 +0300

    Add project files.

---
+++ Kahve/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Basla();
+
+void Basla()
+
+
+{
+
+    Console.Clear();
+    Console.WriteLine("İsmin ne");
+
+    string ?ad = Console.ReadLine();
+    //Console.WriteLine("Merhaba" + ad);
+    Console.WriteLine($"Merhaba {ad}");
+    Console.WriteLine("Kaç yaşındasın?");
+
+
+    int yas = Convert.ToInt32(Console.ReadLine());
+    if (yas < 18)
+    {
+        Console.WriteLine($"{ad} Kahveye giremezsin Daha {18 - yas} yılın var");
+
+    }
+    else if (yas >= 18)
+    {
+        Console.WriteLine($"{ad} kahveye hoşgeldin");
+
+    }
+
+    //int bekle = 18 - yas;
+
+    Console.WriteLine("Devam için enter a basın");
+    Console.ReadLine();
+    Basla();
+}
+
+
